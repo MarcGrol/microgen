2a288a161a2c06a807752008f6320dec319b7c04
Marc Grol <mgrol@xebia.com>
2015-02-16 14:53:43 +0100

    Added close and re-open unit test

--- store/filebasedEventStore_test.go
+++ store/filebasedEventStore_test.go
@@ -16,27 +16,48 @@ func TestStore(t *testing.T) {
 	os.Remove(FILENAME)
 
 	store := NewSimpleEventStore()
-	err := store.Open(FILENAME)
-	assert.Nil(t, err)
 
 	{
+        // write and close
+	    err := store.Open(FILENAME)
+	    assert.Nil(t, err)
 		tourCreatedEvent := events.TourCreated{Year: 2015}
 		store.Store(tourCreatedEvent.Wrap())
+	    store.Close()
+    }
 
+    {
+        // write and close
+	    err := store.Open(FILENAME)
+	    assert.Nil(t, err)
 		cyclistCreatedEvent := events.CyclistCreated{
 			Year:        2015,
 			CyclistId:   42,
 			CyclistName: "Lance",
 			CyclistTeam: "Rabo"}
-		store.Store(cyclistCreatedEvent.Wrap())
+		err = store.Store(cyclistCreatedEvent.Wrap())
+	    assert.Nil(t, err)
+		cyclistCreatedEvent2 := events.CyclistCreated{
+			Year:        2016,
+			CyclistId:   43,
+			CyclistName: "Michael Boogerd",
+			CyclistTeam: "Rabo"}
+		err = store.Store(cyclistCreatedEvent2.Wrap())
+	    assert.Nil(t, err)
+	    store.Close()
+    }
 
+    {
+        // read all and close
+	    err := store.Open(FILENAME)
+	    assert.Nil(t, err)
 		envelopes := make([]*events.Envelope, 0, 2)
 		cb := func(envelope *events.Envelope) bool {
 			envelopes = append(envelopes, envelope)
 			return false
 		}
 		store.Iterate(cb)
-		assert.Equal(t, 2, len(envelopes))
+		assert.Equal(t, 3, len(envelopes))
 
 		assert.Equal(t, uint64(1), envelopes[0].SequenceNumber)
 		assert.Equal(t, "tour", envelopes[0].AggregateName)
@@ -45,22 +66,31 @@ func TestStore(t *testing.T) {
 		assert.Equal(t, 2015, envelopes[0].TourCreated.Year)
 
 		assert.Equal(t, uint64(2), envelopes[1].SequenceNumber)
-		assert.Equal(t, "tour", envelopes[0].AggregateName)
-		assert.Equal(t, "2015", envelopes[0].AggregateUid)
+		assert.Equal(t, "tour", envelopes[1].AggregateName)
+		assert.Equal(t, "2015", envelopes[1].AggregateUid)
 		assert.Equal(t, events.TypeCyclistCreated, envelopes[1].Type)
 		assert.Equal(t, 42, envelopes[1].CyclistCreated.CyclistId)
 		assert.Equal(t, "Lance", envelopes[1].CyclistCreated.CyclistName)
 		assert.Equal(t, "Rabo", envelopes[1].CyclistCreated.CyclistTeam)
-	}
 
-	{
-		envelopes := make([]*events.Envelope, 0, 1)
-		cb := func(envelope *events.Envelope) bool {
-			envelopes = append(envelopes, envelope)
+		assert.Equal(t, uint64(3), envelopes[2].SequenceNumber)
+		assert.Equal(t, "tour", envelopes[2].AggregateName)
+		assert.Equal(t, "2016", envelopes[2].AggregateUid)
+		assert.Equal(t, events.TypeCyclistCreated, envelopes[2].Type)
+		assert.Equal(t, 43, envelopes[2].CyclistCreated.CyclistId)
+		assert.Equal(t, "Michael Boogerd", envelopes[2].CyclistCreated.CyclistName)
+		assert.Equal(t, "Rabo", envelopes[2].CyclistCreated.CyclistTeam)
+
+        // quit when found
+        counter := 0
+		countCb := func(envelope *events.Envelope) bool {
+            counter++
 			return true
 		}
-		store.Iterate(cb)
-		assert.Equal(t, 1, len(envelopes))
+		store.Iterate(countCb)
+		assert.Equal(t, 1, counter)
+
+	    store.Close()
 	}
 
 	store.Close()
