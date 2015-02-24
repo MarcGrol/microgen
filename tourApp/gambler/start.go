1ccc28c4639ca8514c36d5e397e6cb3cb17aa256
Marc Grol <mgrol@xebia.com>
2015-02-24 12:38:12 +0100

    Added start command for gambler

---
+++ tourApp/gambler/start.go
@@ -0,0 +1,85 @@
+package gambler
+
+import (
+	"errors"
+	"fmt"
+	"github.com/MarcGrol/microgen/myerrors"
+	"github.com/MarcGrol/microgen/tourApp/events"
+	"github.com/MarcGrol/microgen/tourApp/http"
+	"github.com/gin-gonic/gin"
+	"os"
+	"strconv"
+)
+
+func Start(listenPort int, busAddress string, baseDir string) error {
+	store, err := startStore(baseDir)
+	if err != nil {
+		return err
+	}
+	bus := startBus(busAddress)
+	if bus == nil {
+		return errors.New("Error starting bus")
+	}
+	startHttp(listenPort, NewGamblerCommandHandler(bus, store))
+	return nil
+}
+
+func startStore(baseDir string) (*events.EventStore, error) {
+	dataDir := baseDir + "/" + "data"
+
+	// create dir if not exists
+	err := os.MkdirAll(dataDir, 0777)
+	if err != nil {
+		return nil, err
+	}
+	store := events.NewEventStore(dataDir, "gambler.db")
+	err = store.Open()
+	if err != nil {
+		return nil, err
+	}
+	return store, nil
+}
+
+func startBus(busAddress string) *events.EventBus {
+	return events.NewEventBus("tourApp", "gambler", busAddress)
+}
+
+func startHttp(listenPort int, commandHandler CommandHandler) {
+	engine := gin.Default()
+	api := engine.Group("/api")
+	{
+		api.GET("/gambler/:gamblerUid/year/:year", func(c *gin.Context) {
+			gamblerUid := c.Params.ByName("gamblerUid")
+			year, err := strconv.Atoi(c.Params.ByName("year"))
+			if err != nil {
+				http.HandleError(c, myerrors.NewInvalidInputError(err))
+				return
+			}
+			gambler, err := commandHandler.HandleGetGamblerQuery(gamblerUid,year)
+			if err != nil {
+				http.HandleError(c, err)
+			}
+			c.JSON(200, *gambler)
+		})
+		api.POST("/gambler", func(c *gin.Context) {
+			var command CreateGamblerCommand
+			c.Bind(&command)
+			err := commandHandler.HandleCreateGamblerCommand(command)
+			if err != nil {
+				http.HandleError(c, err)
+			}
+			c.JSON(200, *http.SuccessResponse())
+		})
+		api.POST("gambler/:gamblerUid/year/:year/team", func(c *gin.Context) {
+			var command CreateGamblerTeamCommand
+			c.Bind(&command)
+			err := commandHandler.HandleCreateGamblerTeamCommand(command)
+			if err != nil {
+				http.HandleError(c, err)
+			}
+			c.JSON(200, *http.SuccessResponse())
+		})
+	}
+
+	engine.Run(fmt.Sprintf(":%d", listenPort))
+}
