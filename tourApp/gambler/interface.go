0695e701506804d653626cab295ac649636ea63c
Marc Grol <mgrol@xebia.com>
2015-03-03 08:36:53 +0100

    with formatting

--- tourApp/gambler/interface.go
+++ tourApp/gambler/interface.go
@@ -3,67 +3,41 @@ package gambler
 // Generated automatically by microgen: do not edit manually
 
 import (
-    
-    "github.com/MarcGrol/microgen/tourApp/events"
+	"github.com/MarcGrol/microgen/tourApp/events"
 )
 
 // commands
 
+type CreateGamblerCommand struct {
+	GamblerUid string `json:"gamblerUid" binding:"required"`
+	Name       string `json:"name" binding:"required"`
+	Email      string `json:"email" binding:"required"`
+}
 
-   
-	type CreateGamblerCommand struct {
-	 GamblerUid  string `json:"gamblerUid" binding:"required"`
-	 Name  string `json:"name" binding:"required"`
-	 Email  string `json:"email" binding:"required"`
-	
-	}
-
-    
-  
-
-   
-	type CreateGamblerTeamCommand struct {
-	 GamblerUid  string `json:"gamblerUid" binding:"required"`
-	 Year  int `json:"year" binding:"required"`
-	 CyclistIds [] int `json:"cyclistIds" `
-	
-	}
-
-    
-  
-
-  
-
+type CreateGamblerTeamCommand struct {
+	GamblerUid string `json:"gamblerUid" binding:"required"`
+	Year       int    `json:"year" binding:"required"`
+	CyclistIds []int  `json:"cyclistIds" `
+}
 
 type CommandHandler interface {
-    
-    	
-        HandleCreateGamblerCommand ( command CreateGamblerCommand ) error
-    	
-    
-    	
-        HandleCreateGamblerTeamCommand ( command CreateGamblerTeamCommand ) error
-    	
-    
-    	 
-        HandleGetGamblerQuery (  gamblerUid string,  year int,  ) (*Gambler, error)
-    	
-    
+	HandleCreateGamblerCommand(command CreateGamblerCommand) error
+
+	HandleCreateGamblerTeamCommand(command CreateGamblerTeamCommand) error
+
+	HandleGetGamblerQuery(gamblerUid string, year int) (*Gambler, error)
 }
 
 // events
 
 type EventHandler interface {
-     OnTourCreated ( event events.TourCreated ) error
-     OnCyclistCreated ( event events.CyclistCreated ) error
-    
+	OnTourCreated(event events.TourCreated) error
+	OnCyclistCreated(event events.CyclistCreated) error
 }
 
 type EventApplier interface {
-     ApplyTourCreated ( event events.TourCreated )
-     ApplyGamblerCreated ( event events.GamblerCreated )
-     ApplyCyclistCreated ( event events.CyclistCreated )
-     ApplyGamblerTeamCreated ( event events.GamblerTeamCreated )
-    
+	ApplyTourCreated(event events.TourCreated)
+	ApplyGamblerCreated(event events.GamblerCreated)
+	ApplyCyclistCreated(event events.CyclistCreated)
+	ApplyGamblerTeamCreated(event events.GamblerTeamCreated)
 }
-
--- tourApp/score/interface.go
+++ tourApp/score/interface.go
@@ -3,62 +3,43 @@ package score
 // Generated automatically by microgen: do not edit manually
 
 import (
-    
-    "github.com/MarcGrol/microgen/tourApp/events"
+	"github.com/MarcGrol/microgen/tourApp/events"
 )
 
 // commands
 
-
-   
-	type CreateDayResultsCommand struct {
-	 Year  int `json:"year" binding:"required"`
-	 EtappeId  int `json:"etappeId" binding:"required"`
-	 BestDayCyclistIds [] int `json:"bestDayCyclistIds" `
-	 BestAllroundCyclistIds [] int `json:"bestAllroundCyclistIds" `
-	 BestClimbCyclistIds [] int `json:"bestClimbCyclistIds" `
-	 BestSprintCyclistIds [] int `json:"bestSprintCyclistIds" `
-	
-	}
-
-    
-  
-
-  
-
+type CreateDayResultsCommand struct {
+	Year                   int   `json:"year" binding:"required"`
+	EtappeId               int   `json:"etappeId" binding:"required"`
+	BestDayCyclistIds      []int `json:"bestDayCyclistIds" `
+	BestAllroundCyclistIds []int `json:"bestAllroundCyclistIds" `
+	BestClimbCyclistIds    []int `json:"bestClimbCyclistIds" `
+	BestSprintCyclistIds   []int `json:"bestSprintCyclistIds" `
+}
 
 type CommandHandler interface {
-    
-    	
-        HandleCreateDayResultsCommand ( command CreateDayResultsCommand ) error
-    	
-    
-    	 
-        HandleGetResultsQuery (  gamblerUid string,  ) (*Results, error)
-    	
-    
+	HandleCreateDayResultsCommand(command CreateDayResultsCommand) error
+
+	HandleGetResultsQuery(gamblerUid string) (*Results, error)
 }
 
 // events
 
 type EventHandler interface {
-     OnEtappeCreated ( event events.EtappeCreated ) error
-     OnCyclistCreated ( event events.CyclistCreated ) error
-     OnGamblerCreated ( event events.GamblerCreated ) error
-     OnGamblerTeamCreated ( event events.GamblerTeamCreated ) error
-     OnTourCreated ( event events.TourCreated ) error
-    
+	OnEtappeCreated(event events.EtappeCreated) error
+	OnCyclistCreated(event events.CyclistCreated) error
+	OnGamblerCreated(event events.GamblerCreated) error
+	OnGamblerTeamCreated(event events.GamblerTeamCreated) error
+	OnTourCreated(event events.TourCreated) error
 }
 
 type EventApplier interface {
-     ApplyGamblerScoreCalculated ( event events.GamblerScoreCalculated )
-     ApplyTourCreated ( event events.TourCreated )
-     ApplyEtappeCreated ( event events.EtappeCreated )
-     ApplyCyclistCreated ( event events.CyclistCreated )
-     ApplyGamblerCreated ( event events.GamblerCreated )
-     ApplyGamblerTeamCreated ( event events.GamblerTeamCreated )
-     ApplyEtappeResultsAvailable ( event events.EtappeResultsAvailable )
-     ApplyCyclistScoreCalculated ( event events.CyclistScoreCalculated )
-    
+	ApplyGamblerScoreCalculated(event events.GamblerScoreCalculated)
+	ApplyTourCreated(event events.TourCreated)
+	ApplyEtappeCreated(event events.EtappeCreated)
+	ApplyCyclistCreated(event events.CyclistCreated)
+	ApplyGamblerCreated(event events.GamblerCreated)
+	ApplyGamblerTeamCreated(event events.GamblerTeamCreated)
+	ApplyEtappeResultsAvailable(event events.EtappeResultsAvailable)
+	ApplyCyclistScoreCalculated(event events.CyclistScoreCalculated)
 }
-
