2d00645c1fa6ce0939c039e99691d69ab7cd0487
marc grol <mgrol@mgrolsvps.colo.transip.net>
2015-06-27 10:45:41 +0200

    Add script to start nsq

---
+++ run_nsq.sh
@@ -0,0 +1,46 @@
+#!/bin/sh -x
+
+LOGDIR=/tmp/nsq
+
+mkdir -p ${LOGDIR}
+nohup nsqlookupd > ${LOGDIR}/nsqlookupd.log & 
+nohup nsqd --lookupd-tcp-address=127.0.0.1:4160 > ${LOGDIR}/nsq.log &
+nohup nsqadmin --lookupd-http-address=127.0.0.1:4161 > ${LOGDIR}/nsqadmin.log &
+
+sleep 1
+ps -eaf|grep nsq
+
+# create topics if needed
+
+curl -X POST localhost:4161/topic/create?topic=tourApp_TourCreated
+curl -X POST localhost:4161/topic/create?topic=tourApp_EtappeCreated
+curl -X POST localhost:4161/topic/create?topic=tourApp_CyclistCreated
+curl -X POST localhost:4161/topic/create?topic=tourApp_EtappeResultsCreated
+curl -X POST localhost:4161/topic/create?topic=tourApp_GamblerCreated
+curl -X POST localhost:4161/topic/create?topic=tourApp_GamblerTeamCreated
+curl -X POST localhost:4161/topic/create?topic=tourApp_CyclistScoreCalculated
+curl -X POST localhost:4161/topic/create?topic=tourApp_GamblerScoreCalculated
+curl -X POST localhost:4161/topic/create?topic=tourApp_NewsItemCreated
+
+#
+# stats
+#
+# open url http://localhost:4171/ in browser
+# nsq_stat --topic=mytopic --channel=ch -lookupd-http-address 127.0.0.1:4161
+
+#
+# producers
+#
+# producer
+# curl -d 'hello world 1' 'http://127.0.0.1:4151/put?topic=mytop'c'
+
+#
+# consumer
+#
+
+# consumer
+# nsq_tail --topic=mytopic --lookupd-http-address=127.0.0.1:4161
+# nsq_to_file --topic=test --output-dir=. --lookupd-http-address=127.0.0.1:4161
+
+
+# webinterface see http://localhost:4171/
